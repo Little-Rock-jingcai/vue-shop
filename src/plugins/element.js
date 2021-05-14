863c79e7cd5b71f313d41f92333c49d028db283f
Little Rock <704991467@qq.com>
2021-05-14 19:38:38 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,19 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+
+//按需导入element-ui组件
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message
+} from 'element-ui'
+
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+/* 将弹框组件挂载为vue原型上的一个属性并且自己起个名字为$message
+这样每个组件中都能使用this.$message的一系列弹窗方法 */
+Vue.prototype.$message = Message
--- src/router.js
+++ src/router.js
@@ -1,23 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+const router = new Router({
+    routes: [
+        //重定向路由规则 如果用户访问/地址时，我们通过redirect重定向到/login地址
+        { path: '/', redirect: '/login' },
+        //路由规则 当用户访问/login地址时，我们通过component指定要展示的组件Login
+        { path: '/login', component: Login },
+
+        { path: '/home', component: Home }
+    ]
 })
+
+//为路由对象router挂载beforeEach路由导航守卫
+router.beforeEach((to, from, next) => {
+    //to 代表将要访问的路径
+    //form 代表从哪个路径跳转而来
+    //next 是一个函数,表示放行
+    // next()放行    next('/login) 强制跳转
+
+    //如果用户访问登录页，不需要做权限控制，直接放行
+    if (to.path === '/login') return next()
+
+    //从sessionStorage中获取保存的token值
+    const tokenStr = window.sessionStorage.getItem('token')
+
+    //如果token不存在时，强制跳转到登录页
+    if (!tokenStr) return next('/login')
+    //token存在时直接放行
+    next()
+})
+
+export default router
