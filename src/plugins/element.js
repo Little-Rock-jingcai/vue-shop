960a759e71c46f5d029e4aa25477536345283565
Little Rock <704991467@qq.com>
2021-05-31 18:20:06 +0800

    完成商品功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -33,7 +33,12 @@ import {
     Cascader,
     Alert,
     Tabs,
-    TabPane
+    TabPane,
+    Steps,
+    Step,
+    CheckboxGroup,
+    Checkbox,
+    Upload
 } from 'element-ui'
 
 
@@ -67,7 +72,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
-
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 
 /* 将弹框组件挂载为vue原型上的一个属性并且自己起个名字为$message
 这样每个组件中都能使用this.$message的一系列弹窗方法 */
